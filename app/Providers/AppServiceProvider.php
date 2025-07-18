2a07fa182ad3993b8a470489da1a4cffd1e06b6c
allmight777 <agoliganange15@gmail.com>
2025-07-18 14:43:52 +0100

    resoooo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,37 +2,23 @@
 
 namespace App\Providers;
 
-<<<<<<< HEAD
-=======
 use Illuminate\Routing\UrlGenerator;
->>>>>>> origin/master
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-<<<<<<< HEAD
-     */
-    public function register(): void
-=======
      *
      * @return void
      */
     public function register()
->>>>>>> origin/master
     {
         //
     }
 
     /**
      * Bootstrap any application services.
-<<<<<<< HEAD
-     */
-    public function boot(): void
-    {
-        //
-=======
      *
      * @param UrlGenerator $url
      * @return void
@@ -42,6 +28,5 @@ class AppServiceProvider extends ServiceProvider
         if (env('APP_ENV') == 'production') {
             $url->forceScheme('https');
         }
->>>>>>> origin/master
     }
 }
