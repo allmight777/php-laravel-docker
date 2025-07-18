8451afc2fccd37b822361539d8ff569574f8d141
allmight777 <agoliganange15@gmail.com>
2025-07-18 14:58:18 +0100

    12

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
+
+
 Route::get('register', [RegisterController::class, 'showRegistrationForm'])->name('register');
 Route::post('register', [RegisterController::class, 'register']);
 
