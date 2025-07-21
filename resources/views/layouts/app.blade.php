097e439d58cb27310dfd1d446ec2d5a8676954e9
allmight777 <agoliganange15@gmail.com>
2025-07-21 17:18:04 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>PGESEM | SchoolConnect</title>
+       <link rel="shortcut icon" href="{{ asset('image_2.png') }}" type="image/x-icon">
 
     <!-- Bootstrap 5 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
