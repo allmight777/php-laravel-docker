622c25687a7282d20df35047e580c0dce0b6cb91
allmight777 <agoliganange15@gmail.com>
2025-07-21 17:17:49 +0100

    Update admin.blade.php

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -7,6 +7,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Admin | Espace</title>
 
+       <link rel="shortcut icon" href="{{ asset('image_2.png') }}" type="image/x-icon">
+
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" />
     <!-- Font Awesome -->
