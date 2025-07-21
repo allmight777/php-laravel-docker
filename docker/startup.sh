de224267971bb4b1633694f1727c851d86684819
allmight777 <agoliganange15@gmail.com>
2025-07-21 17:05:26 +0100

    Update startup.sh

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
 
-echo "✅ Lancement de composer install"
+echo "✅ Installation des dépendances Laravel"
 composer install --no-interaction --prefer-dist --optimize-autoloader
 
-echo "✅ Lancement des migrations (avec suppression des tables)"
+echo "✅ Lancement des migrations (fresh + seed)"
 php artisan migrate:fresh --seed --force
 
-echo "✅ Démarrage du service nginx-php-fpm"
+echo "✅ Lancement du serveur nginx/php-fpm"
 /start.sh
